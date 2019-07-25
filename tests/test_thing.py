b328e83edd303bc7710fc15cff76c635a2201cad
Thomas <thomas.pollet@gmail.com>
2019-07-25 14:32:42 +0200

    update tests

--- tests/test_thing.py
+++ tests/test_thing.py
@@ -29,6 +29,7 @@ def test_create_thing(client):
     res = client.patch(f"/thing/{thing_id}", json={"data" : patch_data})
     assert res.status_code == 201
     res = client.patch(f"/thing/{thing_id}", json={"data" : patch_data})
+    assert res.status_code == 201
     result = res.get_json()
     assert result["data"]["attributes"]["name"] == new_name
 
@@ -77,7 +78,8 @@ def test_create_thing(client):
 def test_thing_get_by_name(client):
     thing = ThingFactory(name="something")
 
-    res = client.get("/thing/get_by_name?name=something")
+    q_params = {"name" : "something"}
+    res = client.get("/thing/get_by_name", query_string=q_params)
     assert res.status_code == 200
     assert res.get_json()["data"]["id"] == thing.id
     assert res.get_json()["data"]["attributes"]["name"] == thing.name
@@ -85,12 +87,31 @@ def test_thing_get_by_name(client):
 
 def test_subthing_include_parent(client):
     subthing = SubThingFactory(name="subsomething")
-    res = client.get(f"/subthing/{subthing.id}?include=thing")
+    q_params = {"include" : "thing"}
+    res = client.get(f"/subthing/{subthing.id}", query_string=q_params)
     assert res.status_code == 200
     response_data = res.get_json()
     assert response_data["data"]["id"] == subthing.id
     assert response_data["data"]["attributes"]["name"] == subthing.name
     assert response_data["included"][0]["id"] == subthing.thing.id
+
+
+def test_invalid_jsonapirpc(client):
+    thing = ThingFactory(name="something", description="nothing")
+    res = client.get("/thing/get_by_name")
+    assert res.status_code == 500
+
+def test_thing_get_fields(client):
+    """
+        Test that only the specified fields are returned
+    """
+    thing = ThingFactory(name="something", description="nothing")
+    q_params = {"fields[thing]" : "name"}
+    res = client.get(f"/thing/{thing.id}", query_string=q_params)
+    assert res.status_code == 200
+    assert res.get_json()["data"]["id"] == thing.id
+    assert res.get_json()["data"]["attributes"]["name"] == thing.name
+    assert res.get_json()["data"]["attributes"].get("description") is None
 
 
 def test_relationship(client):
@@ -103,13 +124,104 @@ def test_relationship(client):
 
     res = client.delete(f"/subthing/{subthing.id}/thing/{subthing.thing.id}")
     assert res.status_code == 200
-    print('x'*100)
     res = client.get(f"/subthing/{subthing.id}/thing")
-    print(res.get_json())
 
 
 def test_validate_swagger(client):
     res = client.get(f"/swagger.json")
     assert res.status_code == 200
     swagger_ = res.get_json()
+
+def test_reader(client):
+    reader_name = 'Test Reader'
+    data = {  
+         "attributes": {  
+           "name": reader_name,
+           "dob": "1970-01-09",  
+           "email": "reader_email0",  
+           "comment": ""  
+         },  
+         "type": "People"  
+        }
+
+    res = client.post('/People', json = { "data" : data })
+    assert res.status_code == 201
+    response_data = res.get_json()
+    assert response_data["data"]["attributes"]["name"] == reader_name
+    reader_id = response_data["data"]["id"]
+
+    res = client.get(f'/People/{reader_id}')
+    assert res.status_code == 200
+    response_data = res.get_json()
+    assert response_data["data"]["attributes"]["name"] == reader_name
+
+    data = {  
+         "attributes": {  
+           "name": "Reader 0 Changed Name",  
+           "email": "reader_email0",  
+           "dob" : "1988-08-09",  
+           "comment": ""  
+         },  
+         "id": reader_id,  
+         "type": "People"  
+       }  
+
+    res = client.patch(f'/People/{reader_id}',json={"data" : data})
+    assert res.status_code == 201
+
+    data['id'] = 'invalid id'
+    res = client.patch(f'/People/{reader_id}',json={"data" : data})
+    assert res.status_code == 400
+
+    res = client.get(f'/People/{reader_id}')
+    response_data = res.get_json()
+    assert res.status_code == 200
+    assert response_data["data"]["attributes"] == data["attributes"]
+    
+    q_params = {"page[limit]" : 10, "include":"publisher", "sort":"-publisher_id"}
+    res = client.get('/Books',query_string=q_params)
+    assert res.status_code == 200
+    response_data = res.get_json()
+    book = response_data["data"][0]
+    book_id = response_data["data"][0]["id"]
+    
+    # Add the book with id book_id to the reader.books_read relation
+    data = [{"id": book_id}] 
+    res = client.post(f'/People/{reader_id}/books_read',json = { "data" : data })
+    assert res.status_code == 200
+    response_data = res.get_json()
+    assert len(response_data["data"]) == 1
+    assert response_data["data"][0]["id"] == book_id
+
+    res = client.get(f'/People/{reader_id}/books_read')
+    assert res.status_code == 200
+    response_data = res.get_json()
+    assert len(response_data["data"]) == 1
+    assert response_data["data"][0]["id"] == book_id
+
+    res = client.get(f'/People/{reader_id}/books_read/{book_id}')
+    assert res.status_code == 200
+    response_data = res.get_json()
+    assert response_data["data"]["id"] == book_id
+    assert response_data["links"]["related"].endswith(f"/Books/{book_id}/")
+
+    q_params = {"page[limit]" : 10}
+    res = client.get('/Books',query_string=q_params)
+    assert res.status_code == 200
+    response_data = res.get_json()
+    assert len(response_data["data"]) == 10
+
+    book_ids = [ book["id"] for book in response_data["data"] if book["id"] != book_id ]
+    res = client.patch(f'/People/{reader_id}/books_read',json = { "data" : [{ "id": id } for id in book_ids] })
+    assert res.status_code == 201
+    response_data = res.get_json()
+    
+    books_read = response_data["data"]
+    books_read_ids = [ book["id"] for book in books_read ]
+    #assert book_id in books_read_ids
+    print(books_read_ids)
+    for book_id in book_ids:
+        assert book_id in books_read_ids
+
+
 
