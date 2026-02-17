415322d9eb67782c57f8ad215437ee1844a528de
Thomas Pollet <thomas.pollet@gmail.com>
2026-02-17 06:40:55 +0100

    Rollback unstable relationship list-payload edge test

--- tests/test_relationship.py
+++ tests/test_relationship.py
@@ -77,15 +77,6 @@ def test_patch_publishers_books_list_rejects_dict_payload(client, db_session, mo
     assert "PATCH a TOMANY relationship" in error["detail"]
 
 
-def test_patch_parent_thing_of_subthing_rejects_list_payload(client, db_session, mock_subthing, mock_thing):
-    payload = {"data": [{"id": mock_thing.id, "type": mock_thing._s_type}]}
-    res = client.patch(f"/subthing/{mock_subthing.id}/thing/", json=payload)
-    assert res.status_code == 400
-
-    subthing = db_session.query(models.SubThing).filter(models.SubThing.id == mock_subthing.id).one_or_none()
-    assert subthing.thing.id == mock_subthing.thing.id
-
-
 def test_delete_publishers_books_list_rejects_dict_payload(client, db_session, mock_publisher_with_3_books):
     payload = {"data": {"id": mock_publisher_with_3_books.books[0].id, "type": mock_publisher_with_3_books.books[0]._s_type}}
     res = client.delete(f"/Publishers/{mock_publisher_with_3_books.id}/books", json=payload)
